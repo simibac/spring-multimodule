19e15d6952853eb96e7fddb81729c13d17477274
omer92 <omer.sabanovic@c-b-f-s.com>
2020-05-19 15:07:09 +0200

    Not working solution, but progress

--- core/src/main/java/ch/cbfs/core/config/FlywayModuleAMigrationConfig.java
+++ core/src/main/java/ch/cbfs/core/config/FlywayModuleAMigrationConfig.java
@@ -1,19 +1,31 @@
 package ch.cbfs.core.config;
 
-//import org.flywaydb.core.Flyway;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.stereotype.Component;
 
-//@Component
-//public class FlywayModuleAMigrationConfig implements FlywayMigrationStrategy {
-//
-//    @Override
-//    public void migrate(Flyway flyway) {
-//        System.out.println("foo");
-//        var dataSource = flyway.getConfiguration().getDataSource();
-//        Flyway testModule = Flyway.configure()
-//                .schemas("core")
-//                .locations("db/migration")
-//                .dataSource(dataSource).load();
-//
-//        testModule.migrate();
-//    }
-//}
+import javax.sql.DataSource;
+
+@Component
+public class FlywayModuleAMigrationConfig implements FlywayMigrationStrategy {
+
+    @Override
+    public void migrate(Flyway flyway) {
+        System.out.println("foo");
+        DataSource dataSource = flyway.getConfiguration().getDataSource();
+        Flyway coreModule = Flyway.configure()
+                .schemas("core")
+                .locations("db/migration")
+                .dataSource(dataSource).load();
+
+        Flyway foundationModule = Flyway.configure()
+                .schemas("foundation")
+                .locations("db/migration")
+                .dataSource(dataSource).load();
+
+        coreModule.migrate();
+
+        foundationModule.migrate();
+    }
+}
--- foundation/src/main/java/ch/cbfs/foundation/config/FlywayModuleBMigrationConfig.java
+++ foundation/src/main/java/ch/cbfs/foundation/config/FlywayModuleBMigrationConfig.java
@@ -1,14 +1,19 @@
-package ch.cbfs.foundation.config;
-
+//package ch.cbfs.foundation.config;
+//
 //import org.flywaydb.core.Flyway;
-
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
 //@Configuration
 //public class FlywayModuleBMigrationConfig implements FlywayMigrationStrategy {
 //
 //    @Override
 //    public void migrate(Flyway flyway) {
 //        System.out.println("bar");
-//        var dataSource = flyway.getConfiguration().getDataSource();
+//        DataSource dataSource = flyway.getConfiguration().getDataSource();
 //        Flyway testModule = Flyway.configure()
 //                .schemas("foundation")
 //                .locations("db/migration")
